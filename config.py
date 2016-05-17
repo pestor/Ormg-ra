b9d3c4086d3c5d6d17ff7af6dc780575f313e90c
pestor <petter.storm@gmail.com>
2016-05-17 09:56:41 +0200

    Lagt till regel för snpEff

--- config.py
+++ config.py
@@ -2,6 +2,10 @@
 BASE_PATH= "/mnt/lustre/Home/petter_s/Area4/Snakemake"
 PROJECT_NAME="TEST1"
 
+#Software
+BWA_PATH="/mnt/lustre/Tools/Software/BWA/0.7.8/bin"
+GATK_PATH="/mnt/lustre/Tools/Software/GATK/3.5"
+SAMTOOLS_PATH=""
 
 # ok fred
 IDX = BASE_PATH + "/genome/GrCH38/GRCh38.p2.genome.fa"
